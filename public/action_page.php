2a27ff358dcf6ef96beb82c69ecd59fa5b545883
Rodrigo <rodrigo13nari@gmail.com>
2020-05-08 22:00:42 -0300

    first commit

---
+++ public/action_page.php
@@ -0,0 +1,7 @@
+<?php
+  // The global $_POST variable allows you to access the data sent with the POST method
+  // To access the data sent with the GET method, you can use $_GET
+  $summoner = $_POST['summoner'];
+  echo $summoner;
+
+?>
